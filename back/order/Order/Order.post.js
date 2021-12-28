33b6ef17a53f7856ddd81168e6f14a0f61cd34e8
kelin <ecom.feng@gmail.com>
2021-12-28 16:35:57 +0100

    21-12-28 16:30

--- back/order/Order/Order.post.js
+++ back/order/Order/Order.post.js
@@ -1,10 +1,9 @@
-api = "https://example.com/api/v1/Order";
+api = "https://example.com/api/b1/Order";
 method = "POST";
 
 formData = {
 	"Order": {required: false, type: ObjectId, description: "如果有, 可能是重新下单 或再来一单, 传递给后端生成新的订单后 可以删除原来取消或失败的订单"},
 	"obj": {
-		Shop: {required: true, type: ObjectId, description: "订单所属商店"},
 		OrderProds: [{
 			Prod: {required: true, type: ObjectId, description: "订单中的商品"},
 			quantity: {required: false, type: Number, description: "采购数量"},	// 如果是单品
@@ -13,20 +12,9 @@ formData = {
 				quantity: {required: true, type: Number, description: "采购数量"},
 			}]
 		}],
-		type_ship: {required: true, type: Number, description: "配送方式"},
-		ship_info: {
-			Cita_code: {required: true, type: ObjectId, description: "运送城市的 code"},
-
-			Client_nome: String,
-			company: String,
-			address: String,
-			city: String,
-			state: String,
-			postcode: String,
-			country: String,
-			email: String,
-			phone: String
-		}
+		Client: {required: false, type: ObjectId, description: "客户" },
+		type_paid: {required: false, type: Number, description: "配送方式"},
+		price_paid: {required: false, type: Float, description: "客户付款金额"},
 	}
 }
 
